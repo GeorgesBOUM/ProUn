c56887fb49a86d09f3c89cc7cbcade36d609c1f5
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-04-16 16:49:59 +0200

    create connexion servlet

---
+++ src/main/java/tp/pro/servlets/Connexion.java
@@ -0,0 +1,72 @@
+package tp.pro.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import tp.pro.beans.Utilisateur;
+import tp.pro.forms.ConnexionForm;
+
+/**
+ * Servlet implementation class Connexion
+ */
+@WebServlet(name = "connexion", urlPatterns = { "/connexion" })
+public class Connexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public static final String ATT_USER         = "utilisateur";
+    public static final String ATT_FORM         = "form";
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+    public static final String VUE              = "/WEB-INF/connexion.jsp";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Connexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher( VUE ).forward( request, response );
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /* Préparation de l'objet formulaire */
+        ConnexionForm form = new ConnexionForm();
+
+        /* Traitement de la requête et récupération du bean en résultant */
+        Utilisateur utilisateur = form.connecterUtilisateur( request );
+
+        /* Récupération de la session depuis la requête */
+        HttpSession session = request.getSession();
+
+        /**
+         * Si aucune erreur de validation n'a eu lieu, alors ajout du bean
+         * Utilisateur à la session, sinon suppression du bean de la session.
+         */
+        if ( form.getErreurs().isEmpty() ) {
+            session.setAttribute( ATT_SESSION_USER, utilisateur );
+        } else {
+            session.setAttribute( ATT_SESSION_USER, null );
+        }
+
+        /* Stockage du formulaire et du bean dans l'objet request */
+        request.setAttribute( ATT_FORM, form );
+        request.setAttribute( ATT_USER, utilisateur );
+
+        request.getRequestDispatcher( VUE ).forward( request, response );
+	}
+
+}
